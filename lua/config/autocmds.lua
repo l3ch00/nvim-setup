e3761e29c856635971a378c5a8ab6ce55c2059f2
l3ch00 <leszekjp@gmail.com>
2025-08-01 18:33:48 +0200

    fix issue with Java files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,8 @@
--- Setup our JDTLS server any time we open up a java file
-vim.cmd [[
-    augroup jdtls_lsp
-        autocmd!
-        autocmd FileType java lua require'config.jdtls'.setup_jdtls()
-    augroup end
-]]
+vim.api.nvim_create_autocmd("User", {
+    pattern = "MasonLspInstalled",
+    callback = function(opts)
+        if opts.data.name == "jdtls" then
+            require("config.jdtls").setup_jdtls()
+        end
+    end,
+})
